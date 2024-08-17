f6c80f36d1af8f4312bbef8815e6b25f97def913
Sergei Bogdanov <77158975+syubogdanov@users.noreply.github.com>
2024-08-18 01:01:58 +0300

    feat: rules for GitLab

---
+++ bull_terrier/domain/entities/third_party/gitlab/username.py
@@ -0,0 +1,61 @@
+import sys
+
+from pydantic import AfterValidator
+
+
+if sys.version_info >= (3, 9):
+    from typing import Annotated
+else:
+    from typing_extensions import Annotated
+
+
+_MIN_LENGTH: int = 2
+_MAX_LENGTH: int = 255
+
+
+def _validate(value: str) -> str:
+    """
+    Ensure that a username is well formed.
+
+    Rules
+    -----
+    * https://docs.gitlab.com/ee/user/profile/#change-your-username;
+    * https://docs.gitlab.com/ee/user/reserved_names.html.
+    """
+    if not (_MIN_LENGTH <= len(value) <= _MAX_LENGTH):
+        detail = (
+            f"The GitLab username must be at least {_MIN_LENGTH}"
+            f" and at most {_MAX_LENGTH} characters"
+        )
+        raise ValueError(detail)
+
+    if not all(char.isalnum() or char in {".", "-", "_"} for char in value):
+        detail = (
+            "The GitLab username can only contain ASCII letters,"
+            " digits, hyphens '-', dots '.' and underscores '_'"
+        )
+        raise ValueError(detail)
+
+    if value.startswith(("_", "-", ".")) or value.endswith(("_", "-", ".")):
+        detail = (
+            "The GitLab username cannot start or end with"
+            " underscore '_', hyphen '-' or dot '.'"
+        )
+        raise ValueError(detail)
+
+    if not value[0].isalnum():
+        detail = "The GitLab username must start with a letter ('a-zA-Z') or digit ('0-9')"
+        raise ValueError(detail)
+
+    if any(f"{char}{char}" in value for char in ("_", "-", ".")):
+        detail = "The GitLab username must not contain consecutive special characters"
+        raise ValueError(detail)
+
+    if value.endswith((".git", ".atom")):
+        detail = "The GitLab username cannot end in '.git' or '.atom'"
+        raise ValueError(detail)
+
+    return value
+
+
+Username = Annotated[str, AfterValidator(_validate)]
