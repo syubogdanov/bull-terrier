bbcf75fa415edcceed7300c94c85d8fad3dcdaa3
Sergei Bogdanov <77158975+syubogdanov@users.noreply.github.com>
2024-09-17 00:31:49 +0300

    feat: delete the test untill the documentation is ready

--- tests/utils/core/root.py
+++
@@ -1,8 +0,0 @@
-from functools import lru_cache
-from pathlib import Path
-
-
-@lru_cache(maxsize=None)
-def get_root() -> "Path":
-    """Get the root directory of `Bull-Terrier`."""
-    return Path(__file__).parent.parent.parent.parent
